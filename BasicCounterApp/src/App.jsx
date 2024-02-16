5040ef4e6b1bb9be1c513beb2c177876d23ade6b
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-02-16 15:07:31 +0530

    counter App

---
+++ BasicCounterApp/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MyComponent() {
+  const [count, setCount] = useState(0)
+  let handleClick =()=>{
+    setCount(count+1)
+  }
+  return(
+    <div>
+          <p>this is count{count}</p>
+          <button onClick={handleClick}> updateCount</button>
+    </div>
+  )
+}
+function App() {
+  return (
+    <>
+       <h1>hello vinay </h1>   
+        <MyComponent />
+
+    </>
+  )
+}
+
+export default App
